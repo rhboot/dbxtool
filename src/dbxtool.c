8b2b2791d3bb070efd45ad9312c75f09c97949b8
Peter Jones <pjones@redhat.com>
2014-03-24 17:23:54 -0400

    Make an iterator and implement "list" with it.
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- src/dbxtool.c
+++ src/dbxtool.c
@@ -26,21 +26,174 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include "esl.h"
 #include "util.h"
+
+#define ACTION_LIST	0x1
+
+typedef struct {
+	char *dbx_file;
+	int action;
+} dbxtool_ctx;
+
+static void
+print_hex(uint8_t *data, size_t len)
+{
+	char hex[] = "0123456789abcdef";
+	for (int i = 0; i < len; i++)
+		printf("%c%c", hex[(data[i] & 0xf0) >> 4],
+			       hex[(data[i] & 0x0f) >> 0]);
+}
+
+typedef struct {
+	uint8_t *buf;
+	size_t len;
+
+	off_t offset;
+	int line;
+
+	EFI_SIGNATURE_LIST *esl;
+	EFI_SIGNATURE_DATA *esd;
+	size_t nmemb;
+	int i;
+} dbx_iter;
+
+int
+dbx_iter_new(dbx_iter **iter, uint8_t *buf, size_t len)
+{
+	if (len < sizeof (EFI_SIGNATURE_LIST) + sizeof (EFI_SIGNATURE_DATA)) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	*iter = calloc(1, sizeof (dbx_iter));
+	if (!*iter)
+		err(1, NULL);
+
+	(*iter)->buf = buf;
+	(*iter)->len = len;
+	(*iter)->i = -1;
+
+	return 0;
+}
+
+int
+dbx_iter_end(dbx_iter *iter)
+{
+	if (!iter) {
+		errno = EINVAL;
+		return -1;
+	}
+	free(iter);
+	return 0;
+}
+
+int
+dbx_iter_next(dbx_iter *iter, efi_guid_t *type, efi_guid_t *owner,
+		uint8_t **data, size_t *len)
+{
+	if (!iter)
+		return -EINVAL;
+	if (iter->offset >= iter->len)
+		return -EINVAL;
+
+	iter->i += 1;
+	iter->line += 1;
+	if (!iter->esl) {
+		iter->esl = (EFI_SIGNATURE_LIST *)iter->buf;
+		iter->i = 0;
+
+		iter->nmemb = (iter->esl->SignatureListSize
+			       - sizeof (EFI_SIGNATURE_LIST)
+			       - iter->esl->SignatureHeaderSize)
+			      / iter->esl->SignatureSize;
+
+		iter->esd = (EFI_SIGNATURE_DATA *)((intptr_t)iter->esl
+			     + sizeof (EFI_SIGNATURE_LIST)
+			     + iter->esl->SignatureHeaderSize);
+	} else if (iter->i == iter->nmemb) {
+		iter->offset += iter->esl->SignatureListSize;
+		if (iter->offset >= iter->len)
+			return 1;
+		iter->esl = (EFI_SIGNATURE_LIST *)((intptr_t)iter->buf
+						+ iter->offset);
+		iter->i = 0;
+		iter->nmemb = (iter->esl->SignatureListSize
+			       - sizeof (EFI_SIGNATURE_LIST)
+			       - iter->esl->SignatureHeaderSize)
+			      / iter->esl->SignatureSize;
+		iter->esd = (EFI_SIGNATURE_DATA *)((intptr_t)iter->esl
+			     + sizeof (EFI_SIGNATURE_LIST)
+			     + iter->esl->SignatureHeaderSize);
+	} else {
+		iter->offset += iter->esl->SignatureSize;
+		iter->esd = (void *)((intptr_t)iter->esd
+				+ iter->esl->SignatureSize);
+	}
+
+	*type = iter->esl->SignatureType;
+	*owner = iter->esd->SignatureOwner;
+	*data = iter->esd->SignatureData;
+	*len = iter->esl->SignatureSize - sizeof (iter->esd->SignatureOwner);
+
+	return 0;
+}
+
+int
+dump_dbx(uint8_t *buf, size_t len)
+{
+	int rc;
+	dbx_iter *iter = NULL;
+
+	rc = dbx_iter_new(&iter, buf, len);
+	if (rc < 0)
+		err(1, NULL);
+
+	while (1) {
+		efi_guid_t type;
+		efi_guid_t owner;
+		uint8_t *data;
+		size_t datalen;
+
+		rc = dbx_iter_next(iter, &type, &owner, &data, &datalen);
+		if (rc < 0)
+			err(1, NULL);
+		if (rc)
+			break;
+
+
+		char *typestr = NULL;
+		int rc = efi_guid_to_name(&type, &typestr);
+		if (rc < 0)
+			err(1, "bad type guid");
+
+		char *ownerstr;
+		rc = efi_guid_to_name(&owner, &ownerstr);
+		if (rc < 0)
+			err(1, "bad owner guid");
+
+		printf("%4d: \"%s\" \"%s\" ", iter->line, ownerstr, typestr);
+		print_hex(data, datalen);
+		printf("\n");
+	}
+	return 0;
+}
 
 int
 main(int argc, char *argv[])
 {
 	int rc;
-	char *dbx_file = NULL;
-	int do_list = 0;
+	uint32_t action = 0;
 
+	dbxtool_ctx ctx = { 0 };
 	poptContext optCon;
 	struct poptOption options[] = {
 		{NULL, '\0', POPT_ARG_INTL_DOMAIN, "dbxtool" },
 		{"dbx", 'd', POPT_ARG_STRING,
-			&dbx_file, 0, "specify dbx database file", "<dbxfile>"},
-		{"list", 'l', POPT_ARG_VAL, &do_list, 1,
+			&ctx.dbx_file, 0, "specify dbx database file",
+			"<dbxfile>"},
+		{"list", 'l', POPT_ARG_VAL|POPT_ARGFLAG_OR,
+			&action, ACTION_LIST,
 			"list entries in dbx", NULL },
 		POPT_AUTOALIAS
 		POPT_AUTOHELP
@@ -52,32 +205,32 @@ main(int argc, char *argv[])
 
 	rc = poptReadDefaultConfig(optCon, 0);
         if (rc < 0)
-		err(1, "poptReadDefaultConfig failed: %s", poptStrerror(rc));
-	
+		errx(1, "poptReadDefaultConfig failed: %s", poptStrerror(rc));
+
 	while ((rc = poptGetNextOpt(optCon)) > 0)
 		;
-	
+
 	if (rc < -1)
-		err(1, "Invalid argument: \"%s\": %s\n",
+		errx(1, "Invalid argument: \"%s\": %s",
 			poptBadOption(optCon, 0), poptStrerror(rc));
-	
+
 	if (poptPeekArg(optCon))
-		err(1, "Invalid argument: \"%s\"\n",
+		errx(1, "Invalid argument: \"%s\"",
 			poptPeekArg(optCon));
-	
+
 	poptFreeContext(optCon);
 
 	uint8_t *dbx_buffer = NULL;
-	size_t len = 0;
+	size_t dbx_len = 0;
 	uint32_t attributes = 0;
-	if (dbx_file != NULL) {
-		int fd = open(dbx_file, O_RDONLY);
+	if (ctx.dbx_file != NULL) {
+		int fd = open(ctx.dbx_file, O_RDONLY);
 		if (fd < 0)
-			err(1, "Could not open file \"%s\": %m\n", dbx_file);
+			err(1, "Could not open file \"%s\"", ctx.dbx_file);
 
-		rc = read_file(fd, (char **)&dbx_buffer, &len);
+		rc = read_file(fd, (char **)&dbx_buffer, &dbx_len);
 		if (rc < 0)
-			err(1, "Could not read file \"%s\": %m\n", dbx_file);
+			err(1, "Could not read file \"%s\"", ctx.dbx_file);
 
 		close(fd);
 		attributes = EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS|
@@ -85,6 +238,8 @@ main(int argc, char *argv[])
 				EFI_VARIABLE_BOOTSERVICE_ACCESS |
 				EFI_VARIABLE_NON_VOLATILE;
 
+		/* if we get a file that's just dd-ed from sysfs,
+		 * it'll have some attribute bits at the beginning */
 		if ((dbx_buffer[0] &
 				~(EFI_VARIABLE_NON_VOLATILE|
 				  EFI_VARIABLE_BOOTSERVICE_ACCESS|
@@ -96,32 +251,41 @@ main(int argc, char *argv[])
 				dbx_buffer[1] == 0 &&
 				dbx_buffer[2] == 0 &&
 				dbx_buffer[3] == 0 &&
-				len > 4) {
+				dbx_len > 4) {
 			attributes = dbx_buffer[0];
-			uint8_t *tmp = malloc(len - 4);
+			uint8_t *tmp = malloc(dbx_len - 4);
 			if (!tmp)
 				err(1, "%m");
 
-			memmove(tmp, dbx_buffer + 4, len - 4);
+			memmove(tmp, dbx_buffer + 4, dbx_len - 4);
 			free(dbx_buffer);
 			dbx_buffer = tmp;
-			len -= 4;
+			dbx_len -= 4;
 		}
 	} else {
 		if (!efi_variables_supported())
-			err(1, "EFI variables are not supported on this "
-				"machine, and no dbx file was specified\n");
+			errx(1, "EFI variables are not supported on this "
+				"machine, and no dbx file was specified");
 
-		efi_guid_t security_guid = EFI_GUID(0xd719b2cb, 0x3d3a, 0x4596,
-			0xa3bc, 0xda, 0xd0, 0x0e, 0x67, 0x65, 0x6f);
-		
-		rc = efi_get_variable(security_guid, "dbx", &dbx_buffer, &len,
-					&attributes);
+		efi_guid_t security_guid;
+
+		rc = efi_name_to_guid("EFI Security Database", &security_guid);
 		if (rc < 0)
-			err(1, "Could not get dbx variable: %m\n");
+			err(1, "Could not get dbx variable name");
+
+		rc = efi_get_variable(security_guid, "dbx", &dbx_buffer,
+					&dbx_len, &attributes);
+		if (rc < 0)
+			err(1, "Could not get dbx variable");
 	}
 
-	printf("dbx was %zd bytes\n", len);
+	if (action == 0)
+		errx(1, "No action specified");
+
+	if (action & ACTION_LIST) {
+		dump_dbx(dbx_buffer, dbx_len);
+		action &= ~ACTION_LIST;
+	}
 
 	return 0;
 }
