ce2d1a4c10f4a4b9dbd51e5514e45f162b8328d8
Peter Jones <pjones@redhat.com>
2014-04-02 16:05:25 -0400

    Implement filetype guessing.
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- src/dbxtool.c
+++ src/dbxtool.c
@@ -70,16 +70,16 @@ dump_dbx(uint8_t *buf, size_t len)
 
 
 		char *typestr = NULL;
-		int rc = efi_guid_to_name(&type, &typestr);
+		int rc = efi_guid_to_symbol(&type, &typestr);
 		if (rc < 0)
 			err(1, "bad type guid");
 
 		char *ownerstr;
-		rc = efi_guid_to_name(&owner, &ownerstr);
+		rc = efi_guid_to_symbol(&owner, &ownerstr);
 		if (rc < 0)
 			err(1, "bad owner guid");
 
-		printf("%4d: \"%s\" \"%s\" ", esd_iter_get_line(iter),
+		printf("%4d: %s %s ", esd_iter_get_line(iter),
 						ownerstr, typestr);
 		print_hex(data, datalen);
 		printf("\n");
@@ -90,6 +90,119 @@ dump_dbx(uint8_t *buf, size_t len)
 
 	esd_iter_end(iter);
 	return 0;
+}
+
+typedef enum {
+	ft_unknown,
+	ft_dbx,
+	ft_dbx_noattr,
+	ft_append_timestamp,
+	ft_append_monotonic
+} filetype;
+
+static inline int
+is_empty_guid(efi_guid_t *guid)
+{
+	if (memcmp(guid,&efi_guid_empty,sizeof (efi_guid_t)) == 0)
+		return 1;
+	return 0;
+}
+
+static inline int
+guidcmp(efi_guid_t *a, efi_guid_t *b)
+{
+	return memcmp(a, b, sizeof (efi_guid_t));
+}
+
+static filetype
+guess_file_type(uint8_t *buf, size_t buflen)
+{
+	EFI_VARIABLE_AUTHENTICATION_2 va2;
+	EFI_VARIABLE_AUTHENTICATION va;
+
+	efi_guid_t guids[] = {
+		efi_guid_pkcs7_cert,
+		efi_guid_rsa2048_sha256_cert,
+		efi_guid_empty
+	};
+
+	efi_guid_t esl_guids[] = {
+		efi_guid_sha256,
+		efi_guid_rsa2048,
+		efi_guid_rsa2048_sha256,
+		efi_guid_sha1,
+		efi_guid_x509_cert,
+		efi_guid_sha224,
+		efi_guid_sha384,
+		efi_guid_sha512,
+		efi_guid_x509_sha256,
+		efi_guid_empty
+	};
+
+	if (buflen >= sizeof (va2)) {
+		memcpy(&va2, buf, sizeof(va2));
+		for (int i = 0; is_empty_guid(&guids[i]) == 0; i++) {
+			if (!guidcmp(&guids[i], &va2.AuthInfo.CertType)) {
+				//printf("Found EFI_AUTHENTICATION_2\n");
+#if 0
+				printf("time is %4d-%02d-%02d %d:%d:%d\n",
+					va2.TimeStamp.Year,
+					va2.TimeStamp.Month,
+					va2.TimeStamp.Day,
+					va2.TimeStamp.Hour,
+					va2.TimeStamp.Minute,
+					va2.TimeStamp.Second);
+#endif
+				return ft_append_timestamp;
+			}
+		}
+	}
+
+	if (buflen >= sizeof (va)) {
+		memcpy(&va, buf, sizeof(va));
+		for (int i = 0; is_empty_guid(&guids[i]) == 0; i++) {
+			if (!guidcmp(&guids[i], &va2.AuthInfo.CertType)) {
+				//printf("Found EFI_AUTHENTICATION\n");
+				return ft_append_monotonic;
+			}
+		}
+	}
+
+	/* if we get a file that's just dd-ed from sysfs,
+	 * it'll have some attribute bits at the beginning */
+	if ((buf[0] &
+			~(EFI_VARIABLE_NON_VOLATILE|
+			  EFI_VARIABLE_BOOTSERVICE_ACCESS|
+			  EFI_VARIABLE_RUNTIME_ACCESS|
+			  EFI_VARIABLE_HARDWARE_ERROR_RECORD|
+			  EFI_VARIABLE_AUTHENTICATED_WRITE_ACCESS|
+			  EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS|
+			  EFI_VARIABLE_APPEND_WRITE)) == 0 &&
+			buf[1] == 0 &&
+			buf[2] == 0 &&
+			buf[3] == 0 &&
+			buflen > (4 + sizeof (EFI_SIGNATURE_LIST))) {
+		EFI_SIGNATURE_LIST esl;
+		memcpy(&esl, buf + 4, sizeof (EFI_SIGNATURE_LIST));
+		for (int i = 0; is_empty_guid(&guids[i]) == 0; i++) {
+			if (!guidcmp(&esl_guids[i], &esl.SignatureType)) {
+				//printf("Found EFI_SIGNATURE_LIST from sysfs\n");
+				return ft_dbx;
+			}
+		}
+	}
+
+	EFI_SIGNATURE_LIST esl;
+	memcpy(&esl, buf, sizeof (EFI_SIGNATURE_LIST));
+	for (int i = 0; is_empty_guid(&guids[i]) == 0; i++) {
+		if (!guidcmp(&esl_guids[i], &esl.SignatureType)) {
+			//printf("Found EFI_SIGNATURE_LIST not from sysfs\n");
+			return ft_dbx_noattr;
+		}
+	}
+
+	//printf("what the hell\n");
+	return ft_unknown;
 }
 
 int
@@ -146,10 +259,36 @@ main(int argc, char *argv[])
 			err(1, "Could not read file \"%s\"", ctx.dbx_file);
 
 		close(fd);
-		attributes = EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS|
-				EFI_VARIABLE_RUNTIME_ACCESS |
-				EFI_VARIABLE_BOOTSERVICE_ACCESS |
-				EFI_VARIABLE_NON_VOLATILE;
+
+		filetype ft = guess_file_type(dbx_buffer, dbx_len);
+		switch (ft) {
+			case ft_unknown:
+				err(1, "Unknown file type");
+				break;
+			case ft_dbx:
+				attributes = dbx_buffer[0];
+				uint8_t *tmp = malloc(dbx_len - 4);
+				if (!tmp)
+					err(1, "%m");
+
+				memmove(tmp, dbx_buffer + 4, dbx_len - 4);
+				free(dbx_buffer);
+				dbx_buffer = tmp;
+				dbx_len -= 4;
+				break;
+			case ft_dbx_noattr:
+				attributes =
+					EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS |
+					EFI_VARIABLE_RUNTIME_ACCESS |
+					EFI_VARIABLE_BOOTSERVICE_ACCESS |
+					EFI_VARIABLE_NON_VOLATILE;
+				break;
+			default:
+				errno = EINVAL;
+				err(1, "Sorry, can't handle this yet");
+				break;
+		}
+#if 0
 
 		/* if we get a file that's just dd-ed from sysfs,
 		 * it'll have some attribute bits at the beginning */
@@ -165,16 +304,8 @@ main(int argc, char *argv[])
 				dbx_buffer[2] == 0 &&
 				dbx_buffer[3] == 0 &&
 				dbx_len > 4) {
-			attributes = dbx_buffer[0];
-			uint8_t *tmp = malloc(dbx_len - 4);
-			if (!tmp)
-				err(1, "%m");
-
-			memmove(tmp, dbx_buffer + 4, dbx_len - 4);
-			free(dbx_buffer);
-			dbx_buffer = tmp;
-			dbx_len -= 4;
 		}
+#endif
 	} else {
 		if (!efi_variables_supported())
 			errx(1, "EFI variables are not supported on this "
@@ -192,18 +323,24 @@ main(int argc, char *argv[])
 			err(1, "Could not get dbx variable");
 	}
 
-	if (action == 0)
-		errx(1, "No action specified");
+	int ret = 0;
+	if (action == 0) {
+		fprintf(stderr, "No action specified");
+		ret = 1;
+		goto end;
+	}
 
 	if (action & ACTION_LIST) {
 		dump_dbx(dbx_buffer, dbx_len);
 		action &= ~ACTION_LIST;
 	}
 
-	if (ctx.dbx_file == NULL) {
-		if (dbx_buffer)
-			free(dbx_buffer);
+end:
+	if (dbx_buffer)
+		free(dbx_buffer);
+	if (ctx.dbx_file) {
+		free(ctx.dbx_file);
 	}
 
-	return 0;
+	return ret;
 }
--- src/esl.h
+++ src/esl.h
@@ -33,4 +33,55 @@ typedef struct {
 	uint32_t	SignatureSize;
 } EFI_SIGNATURE_LIST;
 
+typedef struct {
+	uint16_t	Year;	/* 1998-20xx */
+	uint8_t		Month;	/* 1-12 */
+	uint8_t		Day;	/* 1-31 */
+	uint8_t		Hour;	/* 0-23 */
+	uint8_t		Minute;	/* 0-59 */
+	uint8_t		Second;	/* 0-59 */
+	uint8_t		pad1;
+	uint32_t	Nanosecond;	/* 0 - 999999999 */
+	int16_t		TimeZone;	/* -1440 to 1440 or 0x7ff */
+	uint8_t		Daylight;	/* bitmask, see below */
+	uint8_t		pad2;
+} __attribute__((aligned (1))) EFI_TIME;
+
+#define EFI_TIME_ADJUST_DAYLIGHT	0x01
+#define EFI_TIME_IN_DAYLIGHT		0x02
+
+#define EFI_UNSPECIFIED_TIMEZONE	0x07ff
+
+typedef struct _EFI_CERT_BLOCK_RSA_2048_SHA256 {
+	efi_guid_t	HashType;
+	uint8_t		PublicKey[256];
+	uint8_t		Signature[256];
+} __attribute__((aligned (1))) EFI_CERT_BLOCK_RSA_2048_SHA256;
+
+typedef struct {
+	uint32_t	dwLenght;
+	uint16_t	wRevision;
+	uint16_t	wCertificateType;
+	uint8_t		bCertificate[0];
+} __attribute__((aligned (1))) WIN_CERTIFICATE;
+
+#define WIN_CERT_TYPE_PKCS_SIGNED_DATA	0x0002
+#define WIN_CERT_TYPE_EFI_PKCS115	0x0ef0
+#define WIN_CERT_TYPE_EFI_GUID		0x0ef1
+
+typedef struct {
+	WIN_CERTIFICATE	Hdr;
+	efi_guid_t	CertType;
+} __attribute__((aligned (1))) WIN_CERTIFICATE_UEFI_GUID;
+
+typedef struct {
+	uint64_t			MonotonicCount;
+	WIN_CERTIFICATE_UEFI_GUID	AuthInfo;
+} __attribute__((aligned (1))) EFI_VARIABLE_AUTHENTICATION;
+
+typedef struct {
+	EFI_TIME			TimeStamp;
+	WIN_CERTIFICATE_UEFI_GUID	AuthInfo;
+} __attribute__((aligned (1))) EFI_VARIABLE_AUTHENTICATION_2;
+
 #endif /* ESL_H */
